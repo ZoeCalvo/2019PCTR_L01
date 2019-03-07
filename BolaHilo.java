83ab9dd09817d66501dbd3ff6ffb9a3a9c31ec57
Jorge <Jorge@LAPTOP-H4IR1N0V>
2019-03-07 14:09:22 +0100

    Clase BolaHilo con los errores corregidos

---
+++ BolaHilo.java
@@ -0,0 +1,33 @@
+import practica2.Ball;
+import practica2.Billiards;
+import practica2.Board;
+
+public class BolaHilo implements Runnable {
+
+	private Board board;
+	private Ball bola;
+
+	public BolaHilo(Board board, Ball bola) {
+		// TODO Auto-generated constructor stub
+		this.board = board;
+		this.bola = bola;
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		//Mientras el hilo no se interrumpe y la bola tiene mayor velocidad de 0 se ejecuta.
+		while (!Thread.currentThread().isInterrupted() && bola.getV() > 0.0) { 
+
+			try {
+				bola.move();
+				board.repaint();
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
+	
+}
